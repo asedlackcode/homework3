08f132d52a6c792632e34001eda46e9d101994f0
asedlack <asedlackcode@gmail.com>
2020-06-05 17:10:57 -0700

    copy btn now works

--- javascript.js
+++ javascript.js
@@ -77,4 +77,27 @@ generateEl.addEventListener("click", function(event) {
     console.log(password);
 });
 
+copy.addEventListener("click", function(event) {
+    event.preventDefault();
+    copyTxt();
+})
+
+function copyTxt() {
+    var copyText = document.getElementById("newPass");
+    // Create new element
+   var el = document.createElement('textarea');
+   // Set value (string to be copied)
+   el.value = copyText.textContent;
+   // Set non-editable to avoid focus and move outside of view
+   el.setAttribute('readonly', '');
+   el.style = {position: 'absolute', left: '-9999px'};
+   document.body.appendChild(el);
+   // Select text inside element
+   el.select();
+   // Copy text to clipboard
+   document.execCommand('copy');
+   // Remove temporary element
+   document.body.removeChild(el);
+    alert("Copied Text");
+};
 
